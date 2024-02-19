cd5fd2dc813c9fd5e555c5dc5061f2653f1dc9df
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-02-19 21:41:51 +0200

    commiting

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""second task"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """first page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """second page"""
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
